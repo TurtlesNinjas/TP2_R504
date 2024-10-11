6c0b54e64fe87c7a12c6e5e5d1c4a82f7f67b7ec
TurtlesNinjas <mathis.gogan@gmail.com>
2024-10-11 12:30:22 +0200

    Partie 2 FrizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,13 @@
-def affiche():
-
-    for nombre in range(1, 101):
+def affiche(n):
+    for nombre in range(1, n+1):
         resultat = ""
-
         if nombre % 3 == 0:
             resultat += "Fizz"
         if nombre % 5 == 0:
             resultat += "Buzz"
+        if nombre % 3 == 0 and nombre % 5 == 0:
+            resultat += "FrisBee"
         print(resultat or nombre)
-affiche()
+
+affiche(15)  
+affiche(100)
